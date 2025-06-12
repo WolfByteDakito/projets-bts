9c2b23feecbe9e37794e4af193d4c3aa2407e6cf
WolfByteDakito <mottner90alexis@gmail.com>
2025-06-12 21:40:31 +0200

    Exo C# - Chaine de caractère - partie 1

---
+++ exos/csharp/chaine_caractere/Ex1/Program.cs
@@ -0,0 +1,8 @@
+﻿string chaine = "La formation SIO contient deux spécialités.";
+
+// Afficher la chaine de caractères, caractères un par un
+Console.WriteLine("Affichage de la chaîne de caractères :");
+foreach (char c in chaine)
+{
+    Console.Write(c + " ");
+}
---
+++ exos/csharp/chaine_caractere/Ex2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Concevez un programme qui demande à l'utilisateur de saisir une chaîne de caractères "t", ainsi que deux indices "i" et "j". Le programme doit ensuite créer une nouvelle chaîne "t'" qui contient la sous-chaîne de "t" allant de l'indice "i" à l'indice "j".
+string t;
+int i, j;
+Console.WriteLine("Veuillez saisir une chaîne de caractères :");
+t = Console.ReadLine();
+Console.WriteLine("Veuillez saisir l'indice de début (i) :");
+i = int.Parse(Console.ReadLine());
+Console.WriteLine("Veuillez saisir l'indice de fin (j) :");
+j = int.Parse(Console.ReadLine());
+if (i < 0 || j >= t.Length || i > j)
+{
+    Console.WriteLine("Indices invalides. Assurez-vous que 0 <= i <= j < longueur de la chaîne.");
+}
+else
+{
+    string tPrime = t.Substring(i, j - i + 1);
+    Console.WriteLine($"La sous-chaîne de t de l'indice {i} à l'indice {j} est : '{tPrime}'");
+}
+
